d50270972a945dbde1e5b8d96b5873f5d0f0496b
d_fukuyama <d.fukuyama@gmail.com>
2021-09-21 06:36:51 +0900

    210921

--- google-home-notifier/pi_webserver.js
+++ google-home-notifier/pi_webserver.js
@@ -1,20 +1,33 @@
 const path = process.argv[2];
 
 const { exec } = require('child_process');
+const { waitForDebugger } = require('inspector');
 
 var svr_prt = 0;
-async function get_ip(){
+async function get_port_number(){
   return new Promise((resolve, reject) => {
-    exec("cat /etc/sysconfig/pi", (err, stdout, stderr) => {
-      svr_prt=stdout.trim().replace('MP3_SERVER_PORT=','');
-      //console.log(svr_prt);
+    if(process.env.HOMEDRIVE != 'C:'){
+      exec("cat /etc/sysconfig/pi", (err, stdout, stderr) => {
+        svr_prt=stdout.trim().replace('MP3_SERVER_PORT=','');
+        //console.log(svr_prt);
+        resolve();
+      });
+    }else{
+      svr_prt=8091;
       resolve();
-    });
+    }
   });
 }
 
+
+function sleep(waitSec) {
+  return new Promise(function (resolve) {
+      setTimeout(function() { resolve() }, waitSec);
+  });
+} 
+
 async function main(){
-  await get_ip();
+  await get_port_number();
   const dataServerPort = svr_prt;
 
   console.log(process.argv);
@@ -37,6 +50,15 @@ async function main(){
     res.render('index', { title: req.params.phrase, message: req.params.phrase});
   });
 
+  app.post('/speech/:phrase', function(req, res) {
+    //const { exec } = require('child_process');
+    exec('node /home/pi/google-home-notifier/go.js tts@' + req.params.phrase + '@', (error, stdout, stderr)=> {
+      console.log(stdout);
+      console.log(error);
+    });
+    res.render('index', { title: req.params.phrase, message: req.params.phrase});
+  });
+
   app.get('/*.mp3', function(req, res) {
     console.log(req.query);
     const file = path + req.url;
@@ -47,6 +69,7 @@ async function main(){
   console.log("boot mp3 server with port no. " +  dataServerPort);
 
   app.listen(dataServerPort);
+
 }
 
 main();
